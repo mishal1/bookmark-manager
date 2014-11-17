19a906540a7c09cd2625333d3065a04fa3168132
Mishal Islam <mishal.islam01@gmail.com>
2014-11-17 12:39:44 +0000

    Added database cleaner

--- server.rb
+++ server.rb
@@ -1,4 +1,4 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'data_mapper'
 
 env = ENV["RACK_ENV"] || "development"
@@ -10,11 +10,6 @@ require './lib/link'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-# class BookmarkManager < Sinatra::Base
-#   get '/' do
-#     'Hello BookmarkManager!'
-#   end
-
-#   # start the server if ruby file executed directly
-#   run! if app_file == $0
-# end
+get '/' do
+	erb :index
+end
