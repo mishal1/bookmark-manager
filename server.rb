f39c764bdb99344e009ae1249d45e0695e6ec652
Mishal Islam <mishal.islam01@gmail.com>
2014-11-18 18:55:00 +0000

    Added in sign in feature

--- server.rb
+++ server.rb
@@ -59,6 +59,23 @@ post '/users' do
 	end
 end
 
+get '/sessions/new' do
+	erb :"sessions/new"
+end
+
+post '/sessions' do
+	email = params[:email]
+	password = params[:password]
+	user = User.authenticate(email, password)
+	if user
+		session[:user_id] = user.id
+		redirect to('/')
+	else
+		flash[:errors]=["The email or password is incorrect"]
+		erb :"sessions/new"
+	end
+end
+
 
 
 
